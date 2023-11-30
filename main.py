799885e5afef77ca1c4db0412debad070c0c19ea
Daniel Martinez <danielmartinez1@csumb.edu>
2023-11-29 17:05:56 -0800

    Daniel test

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 from flask import Flask, render_template, request
-from flask_bootstrap import Bootstrap5
-from datetime import datetime
-import requests, json
-
+import requests
 
 app = Flask(__name__)
-bootstrap = Bootstrap5(app)
-
-# api_date_string = recall_initiation_date
-
-# api_date = datetime.strptime(api_date_string, "%Y-%m-%dT%H:%M:%S")
-
-
+print('hello')
 API_URL = "https://api.fda.gov/food/enforcement.json"
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/search', methods = ['POST'])
+@app.route('/search', methods=['POST'])
 def search():
     user_input = request.form.get('input')
     params = {'search': user_input}
-    
-    response = requests.get(API_URL, params = params)
 
-    try:
+    # Make API request
+    response = requests.get(API_URL, params=params)
+
+    # Check if the request was successful
+    if response.status_code == 200:
         data = response.json()
         recalls = data.get('results', [])
-        return render_template('results.html', recalls = recalls, user_input = user_input)
-    except:
-        return render_template('results.html', no_results = True, user_input = user_input)
+        return render_template('results.html', recalls=recalls, user_input=user_input)
+    else:
+        return f"<h1>No items were found! <h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
