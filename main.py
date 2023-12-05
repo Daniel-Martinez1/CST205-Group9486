33ca8753d0e185712e3334cbb506a3a4fbd187eb
alex <amoralopez@csumb.edu>
2023-12-05 09:32:06 -0800

    commit

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ import requests, json
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
-# api_date_string = recall_initiation_date
 
-# api_date = datetime.strptime(api_date_string, "%Y-%m-%dT%H:%M:%S")
 
 
 API_URL = "https://api.fda.gov/food/enforcement.json"
@@ -18,7 +16,7 @@ API_URL = "https://api.fda.gov/food/enforcement.json"
 def index():
     return render_template('index.html')
 
-@app.route('/search', methods=['POST'])
+@app.route('/search', methods = ['POST'])
 def search():
     user_input = request.form.get('input')
     params = {'search': user_input}
@@ -29,6 +27,18 @@ def search():
        try:
             data = response.json()
             recalls = data.get('results', [])
+            for recall in recalls:
+                datestr = recall['report_date']
+                year = datestr[0:4]
+                month = datestr[5:6]
+                day = datestr[6:8]
+            reportDate = f"{month}/{day}/{year}"
+            for recall in recalls:
+                datestr = recall['recall_initiation_date']
+                year = datestr[0:4]
+                month = datestr[5:6]
+                day = datestr[6:8]
+            recallDate = f"{month}/{day}/{year}"
             return render_template('results.html', recalls = recalls, user_input = user_input)
        except:
             pass
