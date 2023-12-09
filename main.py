81c087d8702ba134ea7581aa7cbf6b26b4d9a77d
Daniel Martinez <danielmartinez1@csumb.edu>
2023-12-09 14:06:38 -0800

    Updating main to show more results

--- main.py
+++ main.py
@@ -21,8 +21,10 @@ def index():
 @app.route('/search', methods = ['POST'])
 def search():
     user_input = request.form.get('input')
-    params = {'search': user_input}
-    
+    page = int(request.args.get('page', 1))
+    per_page = 10
+    params = {'search': user_input, 'limit': per_page, 'skip': (page - 1) * per_page}
+
     response = requests.get(API_URL, params = params)
 
     try:
@@ -30,7 +32,7 @@ def search():
         recalls = data.get('results', [])
         for recall in recalls:
            datestr = recall['report_date']
-           year = datestr[0:4]
+           year = datestr[0:4]  
            month = datestr[5:6]
            day = datestr[6:8]
         reportDate = f"{month}/{day}/{year}"
@@ -40,6 +42,7 @@ def search():
            month = datestr[5:6]
            day = datestr[6:8]
         recallDate = f"{month}/{day}/{year}"
-        return render_template('results.html', recalls = recalls, user_input = user_input, recallDate =  recallDate, reportDate = reportDate)
+        print(recall)
+        return render_template('results.html', recalls = recalls, user_input = user_input, recallDate =  recallDate, reportDate = reportDate, page=page)
     except:
         return render_template('results.html', no_results = True, user_input = user_input)
