2e254f091de102ba2894105938c0675b7ce7c376
alex <amoralopez@csumb.edu>
2023-12-10 22:51:04 -0800

    updated search

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
 API_URL = "https://api.fda.gov/food/enforcement.json"
-PRODUCT_API_URL = "https://world.openfoodfacts.org/api/v2/product/737628064502.json"
-
 
 @app.route('/index')
 def index():
@@ -18,14 +16,15 @@ def index():
 def home():
     return render_template('home.html')
 
-
-
 # Daniel Martinez
 @app.route('/search', methods = ['POST'])
 def search():
-    user_input = request.form.get('input')
-    params = {'search': user_input}
-    
+    state = request.form.get('state')
+    city = request.form.get('city')
+    recall_item = request.form.get('recall')
+    user_input = str(state) + str(city) + str(recall_item)
+    params = {'search':input, 'limit': 20, 'skip': 0}
+    user_input = f'{state} {city} {recall_item}'
     response = requests.get(API_URL, params = params)
 
     try:
@@ -34,17 +33,19 @@ def search():
         for recall in recalls:
            datestr = recall['report_date']
            year = datestr[0:4]
-           month = datestr[5:6]
+           month = datestr[4:6]
            day = datestr[6:8]
-        reportDate = f"{month}/{day}/{year}"
+           reportDate = f"{month}/{day}/{year}"
+           recall['report_date'] = reportDate
         for recall in recalls:
            datestr = recall['recall_initiation_date']
            year = datestr[0:4]
-           month = datestr[5:6]
+           month = datestr[4:6]
            day = datestr[6:8]
-        recallDate = f"{month}/{day}/{year}"
+           recallDate = f"{month}/{day}/{year}"
+           recall['recall_initiation_date'] = recallDate
+        print(recalls)
         return render_template('results.html', recalls = recalls, user_input = user_input, recallDate =  recallDate, reportDate = reportDate)
     except:
         return render_template('results.html', no_results = True, user_input = user_input)
-# Daniel Martinez
 
